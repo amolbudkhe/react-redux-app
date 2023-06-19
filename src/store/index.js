269fde5493d10c2936e6043e9eec10ddc898a0ab
amolbudkhe <abudkhe>
2023-06-19 13:19:09 +0530

    implemented class base componente for redux

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "increaseByFive") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+
+  if (action.type === "decreaseByFive") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
